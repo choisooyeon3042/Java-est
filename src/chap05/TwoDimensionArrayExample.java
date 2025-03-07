21e2f88844895c712adba72feef11746a192f430
tndus3042 <sychoi1101@naver.com>
2025-03-07 16:54:54 +0900

    push 확인

--- src/chap05/TwoDimensionArrayExample.java
+++ src/chap05/TwoDimensionArrayExample.java
@@ -7,7 +7,7 @@ public class TwoDimensionArrayExample {
                 {{1, 2, 3},
                  {3, 4, 5}};
         //      ||
-        /*
+
         arrays[0][0] = 1;
         arrays[0][1] = 2;
         arrays[0][2] = 3;
@@ -15,7 +15,7 @@ public class TwoDimensionArrayExample {
         arrays[1][0] = 3;
         arrays[1][1] = 4;
         arrays[1][2] = 5;
-        */
+
 
     }
 }
